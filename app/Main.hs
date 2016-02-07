71975a19bfad6125c0b7d7550f4794d54b684314
Tobias G. Waaler <tobiasgwaaler@gmail.com>
2016-02-07 14:56:10 +0100

    log all requests to stdout

--- app/Main.hs
+++ app/Main.hs
@@ -4,31 +4,30 @@ module Main where
 
 import Network.HTTP.ReverseProxy
 import Network.Wai.Handler.Warp
+import Network.Wai.Middleware.RequestLogger (logStdout)
 import Network.Wai
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Control.Monad.Trans (liftIO)
 import Data.CaseInsensitive (CI)
 import Data.ByteString (ByteString)
 
-import Debug.Trace
+destination = "www.example.com"
+hostHeader = ("Host" :: CI ByteString, destination)
 
-target = "www.example.com"
-hostHeader = ("Host" :: CI ByteString, target)
-
-addHostHeader :: Request -> Request
-addHostHeader req =
+setHostHeaderToDestination :: Request -> Request
+setHostHeaderToDestination req =
   let oldHeaders = requestHeaders req
-      oldHeadersWithouHost = filter (\(key,_) -> not (key == "Host")) oldHeaders
+      oldHeadersWithouHost = filter (\(key,_) -> key /= "Host") oldHeaders
       newHeaders = oldHeadersWithouHost ++ [hostHeader]
-   in traceShowId $ req {requestHeaders = newHeaders}
+   in req {requestHeaders = newHeaders}
 
 main :: IO ()
 main = do
   manager <- newManager defaultManagerSettings
-  run 3001 $
+  run 3001 $ logStdout $
     waiProxyToSettings
       (\ req -> return (WPRModifiedRequest
-                         (addHostHeader req)
-                         (ProxyDest target 80)))
+                         (setHostHeaderToDestination req)
+                         (ProxyDest destination 80)))
       def
       manager
